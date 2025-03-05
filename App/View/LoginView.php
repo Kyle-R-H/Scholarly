c4482447e8ab4147b6031f824552cef0d2869f92
LM-22347526 <22347526@studentmail.ul.ie>
2025-03-05 17:27:34 +0000

    Started LoginView

--- App/View/LoginView.php
+++ App/View/LoginView.php
@@ -1,6 +1,28 @@
-<?php
-namespace App\View;
-use App\Model\UserModel;
-class LoginView extends UserModel{}
-echo "This is LoginView for text, images, etc."
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Scholarly Login</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="custom.css" rel="stylesheet">
+    </head>
+
+    <body style="background-image:url(https://external-preview.redd.it/MP9utSRDTJDOyq77VarXx3fQiEok_fx1pZ_D0ikX_a4.png?format=pjpg&auto=webp&s=465237a4c41c6cba0f22fffeca32ea6471f066b0);">
+        <div style="margin-top:30vh;"  class="d-flex justify-content-center">
+            <div class="form-signin">
+                <h1>Log in</h1>
+                <div class="form-floating mb-3" style="background-size: auto,25px";>
+                    <input id="emailInput" type="email" class="form-control" placeholder="name@ul.ie">
+                    <label for="emailInput">UL email address</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <input id="passwordInput" type="password" class="form-control" placeholder="Password">
+                    <label for="passwordInput">Password</label>
+                </div>
+                <div class="pt-3">
+                    <button class="position-absolute start-50 translate-middle button" type="submit">Log in</button>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
