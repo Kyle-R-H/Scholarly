e827d28e518ae14db267af65664ac983ebc8057e
KyleH <22343261@studentmail.ul.ie>
2025-03-13 18:19:09 +0000

    Server Fix
    Updated code to correct for incorrect capitalisation, added commented out database credentials

--- Core/Model.php
+++ Core/Model.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'core/Database.php';
+require_once 'Core/Database.php';
 
 class Model {
     protected $db;
--- Model/UserModel.php
+++ Model/UserModel.php
@@ -1,15 +1,29 @@
 <?php
-require_once __DIR__ . '/../Core/Model.php';
+if (!file_exists('Core/Model.php')) {
+    die("Error: Core/Model.php not found! Check the file path.");
+}
+
+require_once 'Core/Model.php';
+
+// echo "<br> In usermodel";
 
 class UserModel {
     private $db;
 
     public function __construct() {
+        // echo "<br> In USerMOdel ctor";
         $this->db = new Database();
+        // echo "user: " . getUserByEmail('bob@gmail.com');
     }
 
+    // Fetch user email
     public function getUserByEmail($email) {
-        return $this->db->query("SELECT * FROM users WHERE email = ?", [$email])->fetch(PDO::FETCH_ASSOC);
+        echo "<br> in getUSerbyEmial";
+        $query = "SELECT * FROM users WHERE email = ?";
+        echo "<br>" . $query;
+        $userEmail =  $this->db->query($query, [$email])->fetch(PDO::FETCH_ASSOC);
+        echo "user: " . $userEmail;
+        return $userEmail;
     }
 
     // Fetch user by ID
@@ -17,6 +31,7 @@ class UserModel {
         return $this->db->query("SELECT * FROM users WHERE userid = ?", [$userId])->fetch(PDO::FETCH_ASSOC);
     }
 
+    // TODO: Complete
     public function registerUser($name, $email, $password) {
         $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
         $this->db->query("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [$name, $email, $hashedPassword]);
