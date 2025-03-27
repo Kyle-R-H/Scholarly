0e7f8a9bb2a4cc95d13cb3462769fb15738b412e
SeanMul1 <22354166@studentmail.ul.ie>
2025-03-27 11:40:11 +0000

    adding resturant name to the top of BookingView

--- View/User/BookingView.php
+++ View/User/BookingView.php
@@ -79,35 +79,45 @@
         <!-- query from databse output to array-->
 
         <div class="px-5 py-1" style="width: 100%;">
+            <?php if (!empty($items)): ?>
+                <div class="row px-4 pe-lg-0 align-items-center rounded-3 border shadow-lg">
+                    <div class="col-lg-7 p-5 p-lg-5">
+                        <h1 class="display-10 fw-bold lh-1 text-body-emphasis">
+                            <?= htmlspecialchars($items[0]['BusinessName']) ?>
+                        </h1>
+                    </div>
 
-            <?php foreach ($items as $item): ?>
-                <div class="row px-9 pe-lg-0 align-items-center rounded-3 border shadow-lg position-relative">
+                <?php endif; ?>
 
-                    <div class="col-lg-7 p-4 p-lg-4">
-                        <h1 class="display-10 fw-bold lh-1 text-body-emphasis"><?= htmlspecialchars($item['ItemName']) ?></h1>
-                        <p class="lead"><?= htmlspecialchars($item['Description']) ?></p>
-                        <p><strong>Price: </strong>$<?= number_format($item['ItemPrice'], 2) ?></p>
-                        <div class="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3">
 
-                            <!-- <a href="<?= '?controller=user&action=bookingView&businessName=' . htmlspecialchars($item['BusinessName']) ?>">
+                <?php foreach ($items as $item): ?>
+                    <div class="row px-9 pe-lg-0 align-items-center rounded-3 border shadow-lg position-relative">
+
+                        <div class="col-lg-7 p-4 p-lg-4">
+                            <h1 class="display-10 fw-bold lh-1 text-body-emphasis"><?= htmlspecialchars($item['ItemName']) ?></h1>
+                            <p class="lead"><?= htmlspecialchars($item['Description']) ?></p>
+                            <p><strong>Price: </strong>$<?= number_format($item['ItemPrice'], 2) ?></p>
+                            <div class="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3">
+
+                                <!-- <a href="<?= '?controller=user&action=bookingView&businessName=' . htmlspecialchars($item['BusinessName']) ?>">
                                 <button type="button" class="btn btn-lg px-4">Menu</button>
                             </a> -->
-                        </div>
-                        <div style="right: 0; height: 100%;">
-                            <button type="button" class="btn btn-lg position-absolute top-0 end-0 m-3">
-                                +
-                            </button>
+                            </div>
+                            <div style="right: 0; height: 100%;">
+                                <button type="button" class="btn btn-lg position-absolute top-0 end-0 m-3">
+                                    +
+                                </button>
+
+                            </div>
 
                         </div>
-
-                    </div>
-                    <!-- <div class="col-lg-4 offset-lg-1 p-0 overflow-hidden shadow-lg">
+                        <!-- <div class="col-lg-4 offset-lg-1 p-0 overflow-hidden shadow-lg">
                         <img class="rounded-lg-3" src="<?= htmlspecialchars($item['Image']) ?>" alt="" height="320">
                     </div> -->
-                </div>
-                <hr>
-            <?php endforeach; ?>
+                    </div>
+                    <hr>
+                <?php endforeach; ?>
 
-        </div>
+                </div>
 
 </html>
