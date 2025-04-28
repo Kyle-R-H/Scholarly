f6f088f8f4f27464da1f09f77021bf63bac168a8
KyleH <22343261@studentmail.ul.ie>
2025-04-28 15:01:09 +0100

    Website Headers

--- View/User/UserProfileView.php
+++ View/User/UserProfileView.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <title>User Profile</title>
+    <title>Profile</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="Public/css/Styles.css" rel="stylesheet">
